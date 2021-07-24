cdb82e15e9943b3fc91bdea14335e28d4f8f80b8
Tyler Hawkins <thawkin3@byu.net>
2021-07-23 19:44:22 -0600

    feat: sanitizes input and links to Quora search

--- static/script.js
+++ static/script.js
@@ -1,14 +1,21 @@
 (() => {
+  const sanitizeHTML = (str) => str.replace(/[^\w. ]/gi, (c) => '&#' + c.charCodeAt(0) + ';');
+
   const questionForm = document.querySelector('#questionForm')
   const resultsContainer = document.querySelector('#resultsContainer')
+  const resultsHeader = document.querySelector('#resultsHeader')
   const resultsTableBody = document.querySelector('#resultsTableBody')
 
   questionForm.addEventListener('submit', event => {
     event.preventDefault()
 
-    resultsContainer.style.display = "none"
+    resultsContainer.style.display = 'none'
 
-    fetch(`/api/search?question=${questionForm.question.value}`)
+    const sanitizedUserInput = sanitizeHTML(questionForm.question.value)
+
+    resultsHeader.innerHTML = `Search results for <a href="https://www.quora.com/search?q=${sanitizedUserInput}" target="_blank" rel="noopener noreferrer">your question</a>:`
+
+    fetch(`/api/search?question=${sanitizedUserInput}`)
       .then(response => response.json())
       .then(data => {
         const tableBodyContent = data.reduce((output, item) => {
@@ -17,7 +24,10 @@
         }, '')
 
         resultsTableBody.innerHTML = tableBodyContent
-        resultsContainer.style.display = "block"
+        resultsContainer.style.display = 'block'
+      })
+      .catch(() => {
+        alert('Whoops! Something went wrong while searching.')
       })
   })
 })()
